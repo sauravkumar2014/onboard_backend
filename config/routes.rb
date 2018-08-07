b79c80bdb3866e5b533568a2c761f7957df77c83
shubham805 <shubham805@gmail.com>
2018-08-08 01:44:28 +0530

    dp1

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :recruit_infos
   devise_for :users,
   			controllers: {
+  				sessions: "users/sessions",
   				registrations: "users/registrations"
   			}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
